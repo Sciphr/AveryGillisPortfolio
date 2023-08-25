1bf099f82a7fc50168078789495bb7ea6e020f77
Jacob Berry <jacobtberry99@gmail.com>
2023-08-25 00:47:13 -0400

    third

--- app/components/getData.js
+++ app/components/getData.js
@@ -1,13 +1,16 @@
 const getData = async () => {
-	const response = await fetch(process.env.URL + "/api/tester", {
-		method: "GET",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		next: {
-			revalidate: 10,
-		},
-	});
+	const response = await fetch(
+		"https://avery-gillis-portfolio.vercel.app/api/tester",
+		{
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			next: {
+				revalidate: 10,
+			},
+		}
+	);
 	const data = await response.json();
 	return data;
 };
