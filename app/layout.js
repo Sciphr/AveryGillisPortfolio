15ccb8bb6aba21f8f7805e6dca89d98b139294c5
Jacob Berry <jacobtberry99@gmail.com>
2023-09-29 14:57:48 -0400

    seo changes

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,20 @@
 import "./globals.css";
 import Head from "next/head";
-// import { Inter } from "next/font/google";
-
-// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
 	title: "Avery Gillis Portfolio",
-	description: "Avery Gillis Portfolio",
+	description: "Avery Gillis' writing and interview portfolio",
+	other: {
+		siteName: "Avery Gillis Portfolio",
+		siteUrl: "https://avery-gillis-portfolio.vercel.app/",
+		"twitter:image":
+			"https://media.licdn.com/dms/image/C4E03AQHsQMqKcFHGgw/profile-displayphoto-shrink_800_800/0/1636463668656?e=2147483647&v=beta&t=LNpk1A1e3nA5sDCu9JhP1twI-F7rqGO4WvHn6fvqx48",
+		"twitter:card": "summary_large_image",
+		"og:url": "https://avery-gillis-portfolio.vercel.app/",
+		"og:image":
+			"https://media.licdn.com/dms/image/C4E03AQHsQMqKcFHGgw/profile-displayphoto-shrink_800_800/0/1636463668656?e=2147483647&v=beta&t=LNpk1A1e3nA5sDCu9JhP1twI-F7rqGO4WvHn6fvqx48",
+		"og:type": "website",
+	},
 };
 
 export default function RootLayout({ children }) {
